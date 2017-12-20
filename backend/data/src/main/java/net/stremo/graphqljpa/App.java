91a2b9ec30264bbb3c1c5980019ea86bd543b30a
Strehler <stephan.strehler@saxsys.de>
2017-12-20 15:50:20 +0100

    building for performance tests

--- backend/data/src/main/java/net/stremo/graphqljpa/App.java
+++ backend/data/src/main/java/net/stremo/graphqljpa/App.java
@@ -9,16 +9,24 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class App {
 
+    static boolean buildData = false;
+
     @Autowired
     private TestDataSetup testDataSetup;
 
     public static void main(String[] args) {
+        if(args.length > 0 && args[0].trim().equals("-buildData"))
+           buildData = true;
+
         SpringApplication.run(App.class, args);
     }
 
     @PostConstruct
     public void createTestData() {
 
-        testDataSetup.run();
+        if(buildData) {
+            System.out.println("Testdaten werden erzeugt");
+            testDataSetup.run();
+        }
     }
 }
